ec408e3b56c818cc0d9e94ad87a88c0529f53cf3
EC2 Default User <ec2-user@ip-172-31-39-191.ap-northeast-1.compute.internal>
2019-08-26 02:48:07 +0000

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
+  
+    ##### リンク
+  get "static_pages/inquiry" => redirect("ttps://www.haradacorp.co.jp/contact/index.php?id=foodmaterial")
+  get "static_pages/harada" => redirect("https://www.haradacorp.co.jp/")
+  get "static_pages/privacy" => redirect("https://www.haradacorp.co.jp/privacy/")
+  
 end
